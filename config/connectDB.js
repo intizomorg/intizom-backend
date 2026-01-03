ba4b6944c0673726840134dde9cd04b274d10f3e
intizom1 <uzbekuzbekistan2025@gmail.com>
2026-01-03 09:40:20 -0800

    fix connectDB await crash

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,21 +1,23 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
+    if (!process.env.MONGO_URI) {
+      console.error("FATAL: MONGO_URI missing");
+      process.exit(1);
+    }
+
     await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000
     });
+
     console.log("MongoDB Atlas connected");
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
     process.exit(1);
   }
 }
-await mongoose.connect(process.env.MONGO_URI, {
-  maxPoolSize: 10,
-  serverSelectionTimeoutMS: 5000,
-  socketTimeoutMS: 45000
-});
 
 module.exports = connectDB;
