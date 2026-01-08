660fcb4f820df32fd685598a03261763ad1c890e
intizom1 <uzbekuzbekistan2025@gmail.com>
2026-01-08 09:50:00 -0800

    Update auth layout and sidebar navigation

--- middlewares/adminIpOnly.js
+++ middlewares/adminIpOnly.js
@@ -7,8 +7,9 @@ module.exports = function adminIpOnly(req, res, next) {
   if (!allowed.length) return next();
 
   const ip =
-    req.headers['x-forwarded-for']?.split(',')[0].trim() ||
-    req.ip ||
+    req.headers['cf-connecting-ip'] ||
+    req.headers['x-real-ip'] ||
+    (req.headers['x-forwarded-for'] || '').split(',')[0].trim() ||
     req.socket.remoteAddress;
 
   if (!allowed.includes(ip)) {
